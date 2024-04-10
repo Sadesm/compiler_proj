a4ed0642c779b1ffb73304a72d5fc12d0fcc4b2a
Sadesm <sadesm637@gmail.com>
2024-04-10 17:20:08 +0330

    فاز اولش تقریبا تموم شده مونده فاز های بعدیش

--- main.py
+++ main.py
@@ -1,29 +1,5 @@
 import re
 
-SAMPLE_CODE = """  sahih bozi; 
-ashar arz;
-ashar           tool;\\\ kjlsk gsursbv 
-arz = begir(ashar);
-tool = begir(ashar);
-agar ( tool == arz ){
-chap ("morba ast");
-chap ("masahat");
-chap ( arz * arz );
-chap ("mohit");
-chap ( arz * 4 );\\*djkhwycgagcd
-
-
-   *\\
-}
-agar ( tool != arz ){
-chap ("mostati ast");
-chap ("masahat");
-chap ( arz * tool );
-chap ("mohit");
-chap ( arz * 2 + tool * 2 );
-}
-"""
-
 tokens = {
     'PRINT': r'chap',
     'INPUT': r'begir',
@@ -54,47 +30,84 @@ tokens = {
     'IDENTIFIER': r'[a-zA-Z_][a-zA-Z0-9_]*',
     'IGNORE': r'(\\\*(.|\n)*\*\\|\\\\.*\n|\s|\t)',
 }
-
 CODE_LIST = []
+length = 0
+not_change_code = ''
 
+
+class Token:
+    def __init__(self, _type, _mohtava, _koja):
+        self.type = _type
+        self.mohtava = _mohtava
+        self.makan = _koja
+
+
+def mohasebe_ja(andaze):
+    global not_change_code
+    if andaze > len(not_change_code):
+        return -1, -1
+    _len = 0
+    for i in range(len(not_change_code.splitlines())):
+        for j in range(len(not_change_code.splitlines()[i])):
+            if _len == andaze:
+                return i, j
+            _len += 1
+        _len += 1
 
 def get_first_token(line, token):
     if re.search(token, line) and re.search(token, line).start() == 0:
         return re.search(token, line).end(), re.search(token, line)
 
-
-def build_code_list(code):
+def build_code_list(code, flag=True):
+    global length
     if code == '':
         print("tamom shode :)")
         return 1
-    current_token = (),
+    current_token = Token(None, None, None)
     flag_find_token = False
     _len = -1
     for token, token_p in list(tokens.items()):
         result = get_first_token(code, token_p)
         if result:
             if result[0] > _len:
-                current_token = token, result[1]
                 _len = result[0]
+                current_token = Token(token, code[:_len], mohasebe_ja(length))
                 flag_find_token = True
-    if current_token[0] == 'IGNORE':
+
+    if current_token.type == 'IGNORE' and flag:
+        length += _len
         return build_code_list(code[_len:])
-    if flag_find_token:
-        print('\x1b[6;30;42m' + f'token: {current_token[0]}' + '\x1b[0m')
-        print(code[:_len])
+
+    if flag_find_token and flag:
+        print('\x1b[6;30;42m' + f'token: {current_token.type}' + '\x1b[0m')
+        print(current_token.mohtava, current_token.makan)
+        CODE_LIST.append(current_token)
+        length += _len
+        return build_code_list(code[_len:])
+
+    if flag:
+        print('\u001b[41m' + f'token: ERROR' + '\x1b[0m')
+        for i in range(len(code)):
+            if build_code_list(code[i:], False) == 0:
+                current_token = Token('ERROR', code[:i], mohasebe_ja(length))
+                print(current_token.mohtava, current_token.makan)
+                _len = i
+                break
+        length += _len
         CODE_LIST.append(current_token)
         return build_code_list(code[_len:])
 
-    print("rerrore code kharabe")
-    print(code)
+    if flag_find_token:
+        return 0
+
     return -1
 
+def tree_code(list_of_token):
+    ...
 
 def compile_code(code: str):
+    global not_change_code
+    not_change_code = code
     if build_code_list(code) == 1:
         # TODO faze baad
         ...
-
-
-if __name__ == '__main__':
-    compile_code(SAMPLE_CODE)
